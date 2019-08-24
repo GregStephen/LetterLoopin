5be610ddda2b82191d2fcafae244c8727f22ae3d
Greg Stephen <GregAStephen@gmail.com>
2019-08-24 00:24:36 -0500

    first commit, and done with lesson lol

---
+++ LetterLoopin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LetterLoopin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string capitalizeThisString(string stringToCapitalize)
+            {
+                return stringToCapitalize.First().ToString().ToUpper() + stringToCapitalize.Substring(1);
+            }
+
+            List<string> LoopThruLetters(string phraseToLoop)
+            {
+                List<string> chars = new List<string>();
+                for(var i = 0; i < phraseToLoop.Length; i ++)
+                {
+                    var repeatedChar = string.Concat(Enumerable.Repeat(phraseToLoop[i], (i + 1)));
+                    var capitalizedString = capitalizeThisString(repeatedChar);
+                    chars.Add(capitalizedString);
+                }
+                return chars;
+            }
+
+            Console.WriteLine("Insert a phrase with no spaces!");
+            var phrase = Console.ReadLine().ToLower();
+            var phraseToPrint = LoopThruLetters(phrase);
+            Console.WriteLine(String.Join("-", phraseToPrint));
+        }
+    }
+}
